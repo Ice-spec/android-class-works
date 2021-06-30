c088922e42b0eb8548a63c254d96526a888f6a1d
Ice-spec <alexanderogbe@gmail.com>
2021-06-30 17:56:20 +0100

    Tabs

---
+++ app/src/main/java/com/alexander/tabs/WhatsAppAdapter.kt
@@ -0,0 +1,33 @@
+package com.alexander.tabs
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.alexander.tabs.views.CallFragment
+import com.alexander.tabs.views.ChatFragment
+import com.alexander.tabs.views.StatusFragment
+
+class WhatsAppAdapter(fm: FragmentManager): FragmentStatePagerAdapter(fm){
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        when (position) {
+            0 -> return "Chats"
+            1 -> return "Status"
+            2 -> return "Calls"
+            else -> return "Chats"
+        }
+    }
+
+    override fun getCount(): Int {
+       return 3
+    }
+
+    override fun getItem(position: Int): Fragment {
+        when (position){
+            0 -> return ChatFragment()
+            1 -> return StatusFragment()
+            2 -> return CallFragment()
+            else -> return ChatFragment()
+        }
+    }
+}
